de3ef989c7e4a1b27d3c353912db61086c09ba18
Tuấn Võ <votuanbk232@gmail.com>
2020-12-19 21:52:20 +0700

    feat: add paging product method

--- eShopSolution.Application/Catalog/Products/IManageProductService.cs
+++ eShopSolution.Application/Catalog/Products/IManageProductService.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using eShopSolution.Application.Catalog.Products.Dtos.Manage;
 
 namespace eShopSolution.Application.Catalog.Products
 {
@@ -11,12 +12,18 @@ namespace eShopSolution.Application.Catalog.Products
     {
         Task<int> Create(ProductCreateRequest request);
 
-        Task<int> Update(ProductEditRequest request);
+        Task<int> Update(ProductUpdateRequest request);
+
+        Task<bool> UpdatePrice(int productId, decimal newPrice);
+
+        Task<bool> UpdateStock(int productId, int addedQuantity);
+
+        Task AddViewcount(int productId);
 
         Task<int> Delete(int productId);
 
         Task<List<ProductViewModel>> GetAll();
 
-        Task<PagedViewModel<ProductViewModel>> GetAllPaging(string keyword, int pageIndex, int pageSize);
+        Task<PagedResult<ProductViewModel>> GetAllPaging(GetProductPagingRequest request);
     }
 }
--- eShopSolution.Application/Catalog/Products/IPublicProductService.cs
+++ eShopSolution.Application/Catalog/Products/IPublicProductService.cs
@@ -3,11 +3,12 @@ using eShopSolution.Application.Dtos;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using eShopSolution.Application.Catalog.Products.Dtos.Public;
 
 namespace eShopSolution.Application.Catalog.Products
 {
     public interface IPublicProductService
     {
-        PagedViewModel<ProductViewModel> GetAllByCategoryId(int categoryId, int pageIndex, int pageSize);
+        PagedResult<ProductViewModel> GetAllByCategoryId(GetProductPagingRequest request);
     }
 }
