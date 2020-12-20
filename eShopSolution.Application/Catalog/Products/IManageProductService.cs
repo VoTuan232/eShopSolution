1239b006e7d6151622f01d4f27a8f7f6bf033929
Tuấn Võ <votuanbk232@gmail.com>
2020-12-20 10:03:12 +0700

    feat: product images manage

--- eShopSolution.Application/Catalog/Products/IManageProductService.cs
+++ eShopSolution.Application/Catalog/Products/IManageProductService.cs
@@ -1,10 +1,12 @@
-﻿using eShopSolution.Application.Catalog.Products.Dtos;
-using eShopSolution.Application.Dtos;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Text;
 using System.Threading.Tasks;
-using eShopSolution.Application.Catalog.Products.Dtos.Manage;
+
+using eShopSolution.ViewModels.Catalog.Products;
+using eShopSolution.ViewModels.Catalog.Products.Manage;
+using eShopSolution.ViewModels.Common;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
 
 namespace eShopSolution.Application.Catalog.Products
 {
@@ -23,5 +25,13 @@ namespace eShopSolution.Application.Catalog.Products
         Task<int> Delete(int productId);
 
         Task<PagedResult<ProductViewModel>> GetAllPaging(GetProductPagingRequest request);
+
+        Task<int> AddImages(int productId, List<IFormFile> files);
+
+        Task<int> RemoveImages(int imageId);
+
+        Task<int> UpdateImage(int imageId, string caption, bool isDefault);
+
+        Task<List<ProductImageViewModel>> GetListImage(int productId);
     }
 }
